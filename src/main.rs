838afe204c2bc28961b174ab83da7ee4d370fb77
Thomas <31560900+0xtlt@users.noreply.github.com>
2022-10-16 22:50:09 +0200

    Remove main

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-use chrono::TimeZone;
-use shopify_api::{get_end_of_support_date, ShopifyAPIVersion};
-
-fn main() {
-    assert_eq!(
-        get_end_of_support_date(&ShopifyAPIVersion::V2023_01),
-        chrono::Utc.ymd(2023, 1, 31).and_hms(23, 59, 59)
-    );
-}
