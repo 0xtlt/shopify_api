f62e51377736bd4c7c59d3c88d9de8c6a6b97622
Thomas <31560900+0xtlt@users.noreply.github.com>
2022-10-19 21:36:13 +0200

    Support vector

--- graphql_to_string/src/lib.rs
+++ graphql_to_string/src/lib.rs
@@ -3,7 +3,6 @@ extern crate proc_macro;
 use std::str::FromStr;
 
 use proc_macro::TokenStream;
-use quote::quote;
 use syn::{self, parse_macro_input, Data, DataEnum, DeriveInput, Field, Path, Type, TypePath};
 
 fn is_vec(field: &Field) -> bool {
@@ -23,16 +22,33 @@ fn is_vec(field: &Field) -> bool {
     }
 }
 
-fn extend_token_stream(obj: &mut TokenStream, ts: TokenStream) {
-    let mut ts = ts;
-    obj.extend(ts);
+/// Function for each upper case letter -> _letter
+/// # Example
+/// ```
+/// let s = "HelloWorld";
+/// let s = to_snake_case(s);
+/// assert_eq!(s, "hello_world");
+/// ```
+fn to_snake_case(s: &str) -> String {
+    let mut res = String::new();
+    for (index, c) in s.chars().enumerate() {
+        if c.is_uppercase() {
+            if index != 0 {
+                res.push('_');
+            }
+
+            res.push(c.to_ascii_lowercase());
+        } else {
+            res.push(c);
+        }
+    }
+    res
 }
 
 // Input is a enum that can contains a vector on enum
 // Output is a string that can be used in a graphql query
 #[proc_macro_derive(ToGraphStringDerive)]
 pub fn to_graph_string_derive(tokens: TokenStream) -> TokenStream {
-    let tok_clone = tokens.clone();
     let input = parse_macro_input!(tokens as DeriveInput);
 
     let name = input.ident;
@@ -45,74 +61,55 @@ pub fn to_graph_string_derive(tokens: TokenStream) -> TokenStream {
     // let mut code = TokenStream::from(tok_clone);
     let mut code = TokenStream::new();
 
-    let mut tmp_code = format!(r#"impl ToGraphString for {name} {{
+    let mut tmp_code = format!(
+        r#"impl ToGraphString for {name} {{
         fn to_graph_string(&self) -> String {{
             match self {{
-            "#, name = name);
-
+            "#,
+        name = name
+    );
 
     for variant in variants_punct.iter() {
-        println!("variant: {:#?}", variant);
         let variant_ident = &variant.ident;
         let variant_fields = &variant.fields;
         let variant_field = variant_fields.iter().next();
 
         if variant_field.is_some() && is_vec(variant_field.unwrap()) {
-            // TODO: do it
+            let variant_ident_name = to_snake_case(&variant_ident.to_string());
+            tmp_code.push_str(&format!(
+                r#"
+                {name}::{variant_ident}(vector_var) => {{
+                    let mut s = String::new();
+                    s.push_str("{variant_ident_name}");
+                    s.push_str(" {{");
+                    for field in vector_var.iter().enumerate() {{
+                        s.push('\n');
+                        s.push_str(&field.1.to_graph_string());
+                    }}
+                    s.push_str("\n }}");
+                    s
+                }}
+            "#,
+                name = name,
+                variant_ident = variant_ident,
+                variant_ident_name = variant_ident_name
+            ));
         } else {
-            tmp_code.push_str(&format!(r#"
-                {name}::{variant_ident} => stringify!({variant_ident}).to_string(),
-            "#, name = name, variant_ident = variant_ident));
+            let variant_ident_name = to_snake_case(&variant_ident.to_string());
+            tmp_code.push_str(&format!(
+                r#"
+                {name}::{variant_ident} => stringify!({variant_ident_name}).to_string(),
+            "#,
+                name = name,
+                variant_ident = variant_ident,
+                variant_ident_name = variant_ident_name
+            ));
         }
-        //     let tmp: TokenStream = quote! {
-        //         impl ToGraphString for #name {
-        //             fn to_graph_string(&self) -> String {
-        //                 let mut result = String::new();
-        //                 match self {
-        //                     #name::#variant_ident(v) => {
-        //                         for i in v.iter() {
-        //                             result.push_str(&i.to_graph_string());
-        //                         }
-        //                     }
-        //                 };
-        //                 result
-        //             }
-        //         }
-        //     }
-        //     .into();
-        //     code.extend(tmp);
-        // } else {
-        //     let tmp: TokenStream = quote! {
-        //         impl ToGraphString for #name {
-        //             fn to_graph_string(&self) -> String {
-        //                 let mut result = String::new();
-        //                 match self {
-        //                     #name::#variant_ident(v) => {
-        //                         result.push_str(&v.to_graph_string());
-        //                     }
-        //                 };
-        //                 result
-        //             }
-        //         }
-        //     }
-        //     .into();
-
-        //     code.extend(tmp);
-        // }
-
-        println!("variant_ident: {:#?}", variant_ident);
     }
 
-    tmp_code.push_str(r#" }
-}
-}
-            "#);
+    tmp_code.push_str(r#"}}}"#);
 
     code.extend(TokenStream::from_str(&tmp_code).unwrap());
-
-    // End code
-    let c = code.clone().to_string();
-    println!("code: {:?}", c);
 
     code
 }
--- src/graphql/composer/global.rs
+++ src/graphql/composer/global.rs
@@ -1,5 +1,5 @@
-use graphql_to_string::ToGraphStringDerive;
 use crate::graphql::composer::ToGraphString;
+use graphql_to_string::ToGraphStringDerive;
 
 #[derive(Clone, Debug, ToGraphStringDerive)]
 pub enum MoneyV2 {
@@ -7,61 +7,8 @@ pub enum MoneyV2 {
     CurrencyCode,
 }
 
-// impl ToGraphString for MoneyV2 {
-//     fn to_graph_string(&self) -> String {
-//         match self {
-//             MoneyV2::Amount => "amount".to_string(),
-//             MoneyV2::CurrencyCode => "currencyCode".to_string(),
-//         }
-//     }
-// }
-
-// pub fn generate_wrapped_object(name: &str, fields: Vec<MoneyV2>) -> String {
-//     let mut s = String::new();
-//     s.push_str(name);
-//     s.push_str(" { ");
-//     for field in fields.iter().enumerate() {
-//         if field.0 > 0 {
-//             s.push('\n');
-//         }
-//         s.push_str(&field.1.to_graph_string());
-//     }
-//     s.push_str(" }");
-//     s
-// }
-
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, ToGraphStringDerive)]
 pub enum PriceRangeV2 {
     MaxVariantPrice(Vec<MoneyV2>),
     MinVariantPrice(Vec<MoneyV2>),
 }
-
-// impl ToGraphString for PriceRangeV2 {
-//     fn to_graph_string(&self) -> String {
-//         match self {
-//             PriceRangeV2::MaxVariantPrice(fields) => {
-//                 let mut s = String::new();
-//                 s.push_str("maxVariantPrice");
-//                 s.push_str(" { ");
-//                 for field in fields.iter().enumerate() {
-//                     if field.0 > 0 {
-//                         s.push('\n');
-//                     }
-//                     s.push_str(&field.1.to_graph_string());
-//                 }
-//                 s.push_str(" }");
-//                 s
-//             }
-//             PriceRangeV2::MinVariantPrice(fields) => {
-//                 let mut s = String::new();
-//                 s.push_str("minVariantPrice");
-//                 s.push_str(" { ");
-//                 for field in fields {
-//                     s.push_str(&field.to_graph_string());
-//                 }
-//                 s.push_str(" }");
-//                 s
-//             }
-//         }
-//     }
-// }
